85db2956e4ea478f9da4e7eab712d17197a1ee73
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2026-03-31 13:29:50 -0500

    github deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/rockauto-rebrand/",
+  base: "/CS-407-RockAuto-Rebrand/",
   plugins: [react()],
 });
