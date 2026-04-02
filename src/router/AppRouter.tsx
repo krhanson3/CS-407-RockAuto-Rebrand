e9d6667da7ef4a75b6d90af57ccb678de6db06b7
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2026-04-02 13:56:47 -0500

    links and image in navbar

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,19 +1,16 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Home from '../pages/Home';
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
 import Search from "../pages/Search";
 import Results from "../pages/Results";
 import Account from "../pages/Account";
 
 export default function AppRouter() {
   return (
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </HashRouter>
-
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/results" element={<Results />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
   );
 }
