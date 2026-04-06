32964ac5e8a9f37ac0def09abf296c17f0a5f904
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2026-04-06 09:27:05 -0500

    reverting home page edits

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,10 @@
-import { Link } from "react-router-dom";
+
 
 export default function Home() {
   return (
     <div style={{ padding: "32px", fontFamily: "Arial, sans-serif" }}>
       <h1>Home</h1>
-      <nav style={{ display: "flex", gap: "16px", marginTop: "16px" }}>
-        <Link to="/search">Search</Link>
-        <Link to="/results">Results</Link>
-        <Link to="/account">Account</Link>
-      </nav>
+      
     </div>
   );
 }
