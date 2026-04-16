93b67e20b008cf4a100d761a798d5e8eebd01222
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2026-04-16 10:50:55 -0500

    home page additions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,12 +13,31 @@ export default function HomePage() {
         </h1>
 
         <div className="home-card">
+          <div className="welcome-message">
+              <span className="welcome-message-title">Finding Parts:</span>
+                <span className="welcome-message-message">
+                  Start your search by entering your vehicle information or by entering a specific part number
+                </span>
+              <span className="welcome-message-title">Placing an Order:</span>
+              <span className="welcome-message-message">
+                  Add items to your cart, enter shipping and payment information, then review your Order
+              </span>
+              <span className="welcome-message-title">Existing Orders:</span>
+              <span className="welcome-message-message">
+                  To view orders, check Order Status and Returns
+              </span>
+              
+          </div>
+        </div>
+
+        <div className="home-card">
+          
           <div className="home-buttons">
             <button
               className="home-btn primary"
               onClick={() => navigate("/search-by-vehicle")}
             >
-              Start Your Search
+              Search by Vehicle Type
             </button>
 
             <button
